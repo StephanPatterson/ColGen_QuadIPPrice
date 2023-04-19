2109ef02ae944ca7554b697e5038d6b769a12262
StephanPatterson <stephan.patterson@lsus.edu>
2023-04-19 11:32:52 -0500

    Initial Commit

---
+++ ColGen_QuadIPPrice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ColGen_QuadIPPrice
+//
+//  Created by StephanP on 4/19/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
